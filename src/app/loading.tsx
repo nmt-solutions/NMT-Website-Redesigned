a0e40d4d145fd6f859662c5f5879bc35ec8a5ce8
NMTSolutions <nanomacrotech.soultions@gmail.com>
2024-10-27 00:17:35 +0530

    loading fallback corrected

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,15 @@
 import LoadingFallback from "@/components/LoadingFallback";
+import MaxWidth from "@/components/MaxWidth";
 import React from "react";
 
 const Loading = () => {
-  return <LoadingFallback />;
+  return (
+    <section className="min-h-[calc(100vh-4rem)]">
+      <MaxWidth className="py-4">
+        <LoadingFallback />
+      </MaxWidth>
+    </section>
+  );
 };
 
 export default Loading;
